ba45252577102325a2701b40922dbfe9d12cdedd
tarotum <onyskiv.maxim@gmail.com>
2019-04-22 19:50:58 +0300

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const ENV = require('dotenv');
 const TodoRoutes = require('./routes/todo');
 
 ENV.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
